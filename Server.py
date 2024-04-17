5352f1c076192e4550ea1e0dbb70c58d04f15f13
yevge <yevgeniysarancha@gmail.com>
2024-04-17 17:09:07 +0200

    init

---
+++ Server.py
@@ -0,0 +1,33 @@
+import threading
+import socket
+import random
+import json
+
+
+ADDR = ('localhost', 50007)
+FORMAT = 'utf-8'
+HEADER = 5
+ACCOUNTS_DATA = r'Server\Accounts.json'
+
+
+SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+SERVER.bind((ADDR))
+
+
+def handle_client(conn, addr):
+    pass
+
+
+def start():
+    SERVER.listen(0)
+    while True:
+        conn, addr = SERVER.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(
+            f'{addr} is connected\nCount of active conection {threading.active_count() - 1}\n')
+
+
+if __name__ == '__main__':
+    print(f'server is starting...')
+    start()
